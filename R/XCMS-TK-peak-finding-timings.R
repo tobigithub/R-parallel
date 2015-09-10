6cde5399bb8f880f2f7e782f2cd55b51c9c752a4
Tobias Kind <tkind@ucdavis.edu>
2015-09-09 17:20:24 -0700

    Create XCMS-TK-peak-finding-timings.R

---
+++ R/XCMS-TK-peak-finding-timings.R
@@ -0,0 +1,181 @@
+### ***********************************************************************
+### Profiling peak finding times for xcms v3.1 (do not use for final results)
+### The scripts times centwave and massifquant with different options for QTOF,
+### or Orbitraps and provides individual timings for each
+### URL: http://metlin.scripps.edu/download/
+### LC/MS and GC/MS Data Analysis and Alignement from netCDF data
+### Author: Colin A. Smith <csmith@scripps.edu>
+### ***********************************************************************
+
+### ***********************************************************************
+### The script assumes xcms is installed via Packages->Install->BioConducor
+###     install.packages("xcms")
+###	install.packages("snow")
+###     source("http://bioconductor.org/biocLite.R")
+###     biocLite()
+###     biocLite("multtest")
+###	biocLite("faahKO")
+###     biocLite("msdata")
+### Copy this file into commandline or open via "Open script" 
+### Set 5 variables in +++ section and run have fun
+### Example TimingScript: Tobias Kind fiehnlab.ucdavis.edu 2015
+### ***********************************************************************
+
+### +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+### These are the 5 variables you have to set for your own datafiles anything else runs automatically
+### Set your working directory under Windows, where your netCDF, mzXML files are stored
+### Organize samples in subdirectories according to their class names WT/GM, Sick/Healthy etc.
+### Important: use "/" not "\" 
+myDir = "Z:/faahKO/inst/cdf/"
+myClass1 = "KO"
+myClass2 = "WT"
+myResultDir = "myAlign"
+myCPUs = 16
+### +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    
+TimeMeSec <- function(x) {
+	t1 <- Sys.time()
+	#evaluate function
+	x
+	t2 <-Sys.time()
+	# return Seconds
+	as.numeric(t2-t1,units = "secs")
+}    
+    
+    ### Start timer for global function
+    GlobalTimeStart <-Sys.time()	
+    
+    ### Garbage Collection
+    gc()
+    
+    ### change working directory to your files, see +++ section
+    setwd(myDir)
+    
+    ### get working directory
+    (WD <- getwd())
+    
+    ### load the xcms package
+    library(xcms)
+    
+    ### when using snow under Windows, RMPI must be removed
+    library (snow)
+    
+    ### you can get help typing the following command at the commandline
+    # ?retcor
+    
+     
+    ### ---- start of peak finding timings
+    
+    ### finds peaks in NetCDF, use number of slaves to distribute
+    ### xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs))
+ 
+    ### finds peaks in NetCDF, use number of slaves to distribute
+    xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs))
+
+    ### HP_QTOF  30ppm (metabonexus) Metabolomics; December 2014, Volume 10, Issue 6, pp 1084-1093
+    HP_QTOF_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=30,peakwidth=c(10,60),prefilter=c(3,1000),snthresh=10,integrate=1,noise=200))
+ 
+    ### HP_QTOF_high 15ppm  (metabonexus)
+    HP_QTOFHigh_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=15,peakwidth=c(10,60),prefilter=c(3,1000),snthresh=10,integrate=1,noise=200))
+
+    ###	"HP_Orbi" 2.5 ppm (metabonexus)
+    HP_Orbi_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=2.5,peakwidth=c(10,60),prefilter=c(3,5000),snthresh=10,integrate=1,noise=200))
+
+    ###	 "UP_QTOF" 30 ppm (metabonexus)
+    UP_QTOF_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=30,peakwidth=c(5,20),prefilter=c(3,1000),snthresh=10,integrate=1,noise=200))
+
+    ###  "UP_QTOF_high" 15 ppm (metabonexus)
+    UP_QTOF_high_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=15,peakwidth=c(5,20),prefilter=c(3,1000),snthresh=10,integrate=1,noise=200))
+
+    ###  "UP_Orbi" 2.5 ppm (metabonexus)
+    UP_Orbi_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=2.5,peakwidth=c(5,20),prefilter=c(3,5000),snthresh=10,integrate=1,noise=200))
+ 
+    ### "mzMatch" Bioinformatics. 2013 Jan 15; 29(2): 281–283.
+    mzMatch_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs, method="centWave",ppm=2, peakwidth=c(10,100), snthresh=5, prefilter=c(3,1000), integrate=1, mzdiff=0.01, verbose.columns=TRUE,fitgauss=FALSE))
+
+    ### "Patti" Nat Protoc. 2012 Feb 16; 7(3): 508–516. 
+    CentWaveGen_xcmsFindPeaksTime =   TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=30,peakwidth=c(10,60),prefilter=c(0,0)))
+ 
+    ### Christopher Conley 
+    massifquant_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="massifquant",consecMissedLimit=1,snthresh=10,criticalValue=1.73, ppm = 10, peakwidth= c(30, 60), prefilter= c(1,3000), withWave = 0))
+    
+ 
+    ### ---- end of peak finding timings
+    
+    ### print used files and memory usuage
+    xset
+    
+    ###  Group peaks together across samples and show fancy graphics 
+    ###  you can remove the sleep timer (silent run) or set it to 0.001 or slow to show(1.0)
+    xcmsGroupTime = TimeMeSec(xset <- group(xset,sleep=.0001))
+ 
+    ### calculate retention time deviations for every time 
+    xcmsRetCorTime = TimeMeSec(xset2 <- retcor(xset, family="s", plottype="m"))
+     
+    ###  Group peaks together across samples, set bandwitdh, change important m/z parameters here
+    ###  Syntax: group(object, bw = 30, minfrac = 0.5, minsamp= 1,  mzwid = 0.25, max = 5, sleep = 0) 
+    xcmsGroupSamplesTime = TimeMeSec(xset2 <- group(xset2, bw =10))
+      
+    ###   identify peak groups and integrate samples
+    xcmsRetFillPeakTime = TimeMeSec(xset3 <- fillPeaks(xset2))
+    
+    ###  print statistics
+    xset3
+    
+    ### ask  par(ask=T) or dont ask  par(ask=F) for confirmation
+    par(ask=F)
+    
+    ### create report and save the result in EXCEL file, print 30 important peaks as PNG 
+    xcmsReportTime = TimeMeSec(reporttab <- diffreport(xset3, myClass1, myClass2, myResultDir, 30, metlin = 0.15))
+    
+    ### print file names
+    # dir(path = ".", pattern = NULL, all.files = FALSE, full.names = FALSE, recursive = FALSE)
+    
+    ### output were done!
+    print("Finished, open by yourself the file myAlign.tsv and pictures in myAlign_eic") 
+
+    GlobalTimeStop <-Sys.time()	
+    GlobalTime = as.numeric(GlobalTimeStop-GlobalTimeStart,units = "secs")
+    
+
+    cat("xcmsFindPeaksTime original: ",xcmsFindPeaksTime,"[s]\n");
+
+    ### HP_QTOF from metabonexus 30ppm
+    cat("HP_QTOF 30 ppm: ", HP_QTOF_xcmsFindPeaksTime,"[s]\n");
+ 
+    ### HP_QTOF_high from metabonexus 15ppm
+    cat("HP_QTOF_high 15 ppm: ", HP_QTOFHigh_xcmsFindPeaksTime,"[s]\n"); 
+
+    ###	"HP_Orbi" 2.5 ppm
+    cat("HP_Orbi 2.5 ppm : ", HP_Orbi_xcmsFindPeaksTime,"[s]\n"); 
+
+    ###	 "UP_QTOF" 30 ppm
+    cat("UP_QTOF 30 ppm : ",UP_QTOF_xcmsFindPeaksTime,"[s]\n"); 
+
+    ###  "UP_QTOF_high" 15 ppm
+    cat("UP_QTOF_high 15 ppm : ", UP_QTOF_high_xcmsFindPeaksTime,"[s]\n"); 
+
+    ###  "UP_Orbi" 2.5 ppm
+    cat("UP_Orbi  2.5 ppm :", UP_Orbi_xcmsFindPeaksTime,"[s]\n");
+     
+    ### "mzMatch 2.0 ppm" 
+    cat("mzMatch 2.0 ppm: ",mzMatch_xcmsFindPeaksTime,"[s]\n");
+
+    ### "Generic 30 ppm"
+    cat("CentWave Generic 30 ppm: ",CentWaveGen_xcmsFindPeaksTime,"[s]\n");
+    
+    ###  Christopher Conley 
+    cat("massifquant 10 ppm: ",massifquant_xcmsFindPeaksTime,"[s]\n");
+
+
+
+cat("xcmsGroupTime: ",xcmsGroupTime,"\n");
+cat("xcmsRetCorTime: ",xcmsRetCorTime,"\n");
+cat("xcmsGroupSamplesTime: ",xcmsGroupSamplesTime,"\n");
+cat("xcmsRetFillPeakTime: ",xcmsRetFillPeakTime,"\n");
+cat("xcmsReportTime: ",xcmsReportTime,"\n");
+cat("GlobalTime: ",GlobalTime,"\n");
+
+print("Finished profiling peakfinding.") 
+print("Warning: Final results are incombatible with RT alignment procedures.") 
