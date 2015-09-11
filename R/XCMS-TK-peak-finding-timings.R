7f7ebea2aeaf0a1fa9f09f20b397fad0743aeee5
Tobias Kind <tkind@ucdavis.edu>
2015-09-10 20:28:38 -0700

    Update XCMS-TK-peak-finding-timings.R

--- R/XCMS-TK-peak-finding-timings.R
+++ R/XCMS-TK-peak-finding-timings.R
@@ -1,5 +1,5 @@
 ### ***********************************************************************
-### Profiling peak finding times for xcms v3.1 (do not use for final results)
+### Profiling peak finding times for xcms v3.1 (no final results)
 ### The scripts times centwave and massifquant with different options for QTOF,
 ### or Orbitraps and provides individual timings for each
 ### URL: http://metlin.scripps.edu/download/
@@ -61,84 +61,82 @@ TimeMeSec <- function(x) {
     ### when using snow under Windows, RMPI must be removed
     library (snow)
     
-    ### you can get help typing the following command at the commandline
-    # ?retcor
-    
-     
     ### ---- start of peak finding timings
-    
-    ### finds peaks in NetCDF, use number of slaves to distribute
-    ### xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs))
- 
+
     ### finds peaks in NetCDF, use number of slaves to distribute
     xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs))
-
+    xset
+    rm(xset)
+    
     ### HP_QTOF  30ppm (metabonexus) Metabolomics; December 2014, Volume 10, Issue 6, pp 1084-1093
     HP_QTOF_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=30,peakwidth=c(10,60),prefilter=c(3,1000),snthresh=10,integrate=1,noise=200))
- 
+    xset
+    rm(xset)
+    
     ### HP_QTOF_high 15ppm  (metabonexus)
     HP_QTOFHigh_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=15,peakwidth=c(10,60),prefilter=c(3,1000),snthresh=10,integrate=1,noise=200))
-
+    xset
+    rm(xset)
+    
     ###	"HP_Orbi" 2.5 ppm (metabonexus)
     HP_Orbi_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=2.5,peakwidth=c(10,60),prefilter=c(3,5000),snthresh=10,integrate=1,noise=200))
-
+    xset
+    rm(xset)
+    
     ###	 "UP_QTOF" 30 ppm (metabonexus)
     UP_QTOF_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=30,peakwidth=c(5,20),prefilter=c(3,1000),snthresh=10,integrate=1,noise=200))
-
+    xset
+    rm(xset)
+    
     ###  "UP_QTOF_high" 15 ppm (metabonexus)
     UP_QTOF_high_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=15,peakwidth=c(5,20),prefilter=c(3,1000),snthresh=10,integrate=1,noise=200))
-
+    xset
+    rm(xset)
+    
     ###  "UP_Orbi" 2.5 ppm (metabonexus)
     UP_Orbi_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=2.5,peakwidth=c(5,20),prefilter=c(3,5000),snthresh=10,integrate=1,noise=200))
- 
+    xset
+    rm(xset)
+   
     ### "mzMatch" Bioinformatics. 2013 Jan 15; 29(2): 281–283.
     mzMatch_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs, method="centWave",ppm=2, peakwidth=c(10,100), snthresh=5, prefilter=c(3,1000), integrate=1, mzdiff=0.01, verbose.columns=TRUE,fitgauss=FALSE))
-
+    xset
+    rm(xset)
+    
     ### "Patti" Nat Protoc. 2012 Feb 16; 7(3): 508–516. 
     CentWaveGen_xcmsFindPeaksTime =   TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave",ppm=30,peakwidth=c(10,60),prefilter=c(0,0)))
+    xset
+    rm(xset)
  
     ### Christopher Conley 
     massifquant_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="massifquant",consecMissedLimit=1,snthresh=10,criticalValue=1.73, ppm = 10, peakwidth= c(30, 60), prefilter= c(1,3000), withWave = 0))
-    
+    xset
+    rm(xset)
  
+    ### "Can we trust metabolomics"
+    TrustMetabolomics_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave", peakwidth=c(3,15), snthresh=5))
+    xset
+    rm(xset)
+
+    
+    ### IPO centwave initial
+    IPOCentwave_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="centWave", peakwidth=c(20,50), 
+    						ppm=c(17,32), mzdiff=c(-0.001, 0.01), snthresh=10, noise=0, prefilter=3,
+    						mzCenterFun="wMean", integrate=1, fitgauss=FALSE, verbose.columns=FALSE)) 
+    xset
+    rm(xset)       
+    
+    ### IPO matchedFilter for lowres data
+    IPOMatchedFilter_xcmsFindPeaksTime = TimeMeSec(xset <- xcmsSet(nSlaves=myCPUs,method="matchedFilter",fwhm=c(25), snthresh=c(3), step=c(0.05), steps=c(2),max=5, mzdiff=0.8))
+    xset
+    rm(xset)
+    
     ### ---- end of peak finding timings
     
-    ### print used files and memory usuage
-    xset
-    
-    ###  Group peaks together across samples and show fancy graphics 
-    ###  you can remove the sleep timer (silent run) or set it to 0.001 or slow to show(1.0)
-    xcmsGroupTime = TimeMeSec(xset <- group(xset,sleep=.0001))
- 
-    ### calculate retention time deviations for every time 
-    xcmsRetCorTime = TimeMeSec(xset2 <- retcor(xset, family="s", plottype="m"))
-     
-    ###  Group peaks together across samples, set bandwitdh, change important m/z parameters here
-    ###  Syntax: group(object, bw = 30, minfrac = 0.5, minsamp= 1,  mzwid = 0.25, max = 5, sleep = 0) 
-    xcmsGroupSamplesTime = TimeMeSec(xset2 <- group(xset2, bw =10))
-      
-    ###   identify peak groups and integrate samples
-    xcmsRetFillPeakTime = TimeMeSec(xset3 <- fillPeaks(xset2))
-    
-    ###  print statistics
-    xset3
-    
-    ### ask  par(ask=T) or dont ask  par(ask=F) for confirmation
-    par(ask=F)
-    
-    ### create report and save the result in EXCEL file, print 30 important peaks as PNG 
-    xcmsReportTime = TimeMeSec(reporttab <- diffreport(xset3, myClass1, myClass2, myResultDir, 30, metlin = 0.15))
-    
-    ### print file names
-    # dir(path = ".", pattern = NULL, all.files = FALSE, full.names = FALSE, recursive = FALSE)
-    
-    ### output were done!
-    print("Finished, open by yourself the file myAlign.tsv and pictures in myAlign_eic") 
-
     GlobalTimeStop <-Sys.time()	
     GlobalTime = as.numeric(GlobalTimeStop-GlobalTimeStart,units = "secs")
     
-
+    ### Original XCMS with faahKO set
     cat("xcmsFindPeaksTime original: ",xcmsFindPeaksTime,"[s]\n");
 
     ### HP_QTOF from metabonexus 30ppm
@@ -165,17 +163,17 @@ TimeMeSec <- function(x) {
     ### "Generic 30 ppm"
     cat("CentWave Generic 30 ppm: ",CentWaveGen_xcmsFindPeaksTime,"[s]\n");
     
-    ###  Christopher Conley 
+    ### massifquant - Christopher Conley 
     cat("massifquant 10 ppm: ",massifquant_xcmsFindPeaksTime,"[s]\n");
 
+    ### "Can we trust metabolomics" Metabolomics August 2015, Volume 11, Issue 4, pp 807-821
+    cat("Trust Metabolomics: ",TrustMetabolomics_xcmsFindPeaksTime,"[s]\n");
 
+    ### IPO centwave initial
+    cat(" IPO centwave initial :",IPOCentwave_xcmsFindPeaksTime,"[s]\n");
 
-cat("xcmsGroupTime: ",xcmsGroupTime,"\n");
-cat("xcmsRetCorTime: ",xcmsRetCorTime,"\n");
-cat("xcmsGroupSamplesTime: ",xcmsGroupSamplesTime,"\n");
-cat("xcmsRetFillPeakTime: ",xcmsRetFillPeakTime,"\n");
-cat("xcmsReportTime: ",xcmsReportTime,"\n");
-cat("GlobalTime: ",GlobalTime,"\n");
+    ### IPO MatchedFilter initial
+    cat(" IPO MatchedFilter initial :",IPOMatchedFilter_xcmsFindPeaksTime,"[s]\n");
 
 print("Finished profiling peakfinding.") 
-print("Warning: Final results are incombatible with RT alignment procedures.") 
+
