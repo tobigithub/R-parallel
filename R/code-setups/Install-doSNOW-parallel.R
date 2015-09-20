6464c31d4ecd7772757e26b6894270c44433757b
Tobias Kind <tkind@ucdavis.edu>
2015-09-20 00:44:10 -0700

    Create Install-doSNOW-parallel.R

---
+++ R/code-setups/Install-doSNOW-parallel.R
@@ -0,0 +1,28 @@
+### Installation of the doSNOW parallel library with all dependencies
+chooseCRANmirror()
+install.packages("doSNOW", dependencies = c("Depends", "Imports")) 
+
+##Loading required package: foreach
+##foreach: simple, scalable parallel programming from Revolution Analytics
+##Use Revolution R for scalability, fault tolerance and more.
+##http://www.revolutionanalytics.com
+##Loading required package: iterators
+##Loading required package: snow
+
+# Cluster
+# load doSnow library
+library(doSNOW)
+
+# Create compute cluster of 4 (try 64)
+# One can increase up to 128 nodes
+# Each node requires 44 Mbyte RAM under WINDOWS.
+cluster = makeCluster(4, type = "SOCK")
+
+# register the cluster
+registerDoSNOW(cluster)
+
+# insert parallel computation here
+
+# stop cluster and remove clients
+stopCluster(cluster)
+
