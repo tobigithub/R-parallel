783387b7cbbdaec7b41c1f1170ecf1b49285e127
Tobias Kind <tkind@ucdavis.edu>
2015-09-20 23:22:00 -0700

    Update doParallel-fire-all-empty-threads.R

--- R/code-benchmarks/doParallel-fire-all-empty-threads.R
+++ R/code-benchmarks/doParallel-fire-all-empty-threads.R
@@ -1,20 +1,39 @@
-    # How long does it take to fire all empty threads
-    library(doParallel);
+### ---------------------------------------------------------------
+### Benchmark for creation of empty doParallel cluster 
+### How long does it take to fire all empty threads
+### Each rscript client require 44 Mb memory, hence 1-2 GByte RAM needed.
+### Tobias Kind (2015) https://github.com/tobigithub/R-parallel/
+### ---------------------------------------------------------------
+
+# Installation of the required library with all dependencies
+doInstall <- TRUE # Change to FALSE if you don't want packages installed.
+toInstall <- c("doParallel") 
+if((doInstall) && (!is.element(toInstall, installed.packages()[,1])))
+{
+	cat("Please install required package. Select server:"); chooseCRANmirror();
+	install.packages(toInstall, dependencies = c("Depends", "Imports")) 
+}
+    
+library(doParallel);
    
-    # how often to repeat the test
-    n = 3
-    Sys.time()->start;
-    for (i in 1:n)
-    { 
-  	  cl <- makeCluster(detectCores()); 
+# how often to repeat the test
+n = 3
+Sys.time()->start;
+for (i in 1:n)
+{ 
+ 	  cl <- makeCluster(detectCores()); 
   	  registerDoParallel(cl);
-  	  getDoParWorkers(); 
+  	  #getDoParWorkers(); 
   	  stopCluster(cl); 
   	  
-    } 
-    cl;
-    t=(Sys.time()-start);
-    cat(as.numeric(t/n)," sec total\n")
-    cat(as.numeric(t/n/32),"sec. per thread\n")	
-    # END
+} 
+cl;
+t=(Sys.time()-start);
+cat(round(as.numeric(t/n),2)," sec total\n")
+cat(round(as.numeric(t/n/detectCores()),2),"sec. per thread\n")	
+    
+# clean up memory
+invisible(gc())
+    
+# END
     
