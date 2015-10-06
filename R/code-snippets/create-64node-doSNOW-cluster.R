8b6aeb1790c75706a16fca898a1611abc7e795f6
Tobias Kind <tkind@ucdavis.edu>
2015-10-05 20:47:16 -0700

    Update create-64node-doSNOW-cluster.R

--- R/code-snippets/create-64node-doSNOW-cluster.R
+++ R/code-snippets/create-64node-doSNOW-cluster.R
@@ -39,6 +39,9 @@ plot(tm)
 # stop the cluster
 stopCluster(cl)
 
+# insert serial backend, otherwise error in repetetive tasks
+registerDoSEQ()
+
 # remove the large x object and clean up memory
 remove(x); gc()
 
